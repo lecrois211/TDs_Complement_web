559cb21070c7d6691fee51686f9ed1ff6bd6495a
Jean-Victor Lecroisey <lecrois211@campus.unicaen.fr>
2023-01-26 14:09:06 +0100

    Version 1 - ajout d'une vue 'footer', ajout d'une vue 'header, ajout d'une vue 'index', ajout d'un controller 'ItemsController'

--- src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
+++ src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
@@ -1,4 +1,14 @@
 package edu.spring.td1.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
 class ItemsController {
+
+    @RequestMapping("/")
+    fun indexAction():String{
+        return "index"
+    }
+
 }
