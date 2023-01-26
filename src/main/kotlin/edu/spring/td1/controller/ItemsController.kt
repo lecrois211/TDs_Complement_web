3aa2cc6e3b827574374395e5b77169d45db6cf6c
Jean-Victor Lecroisey <lecrois211@campus.unicaen.fr>
2023-01-26 15:20:30 +0100

    version 2 - ajout de plusieurs controller, modification de la vue 'index.html', ajout de la vue 'newForm.html'

--- src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
+++ src/main/kotlin/edu/spring/td1/controller/ItemsController.kt
@@ -1,14 +1,44 @@
 package edu.spring.td1.controller
 
-import org.springframework.stereotype.Controller
+import edu.spring.td1.models.Item
+import org.springframework.stereotype.*
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.*
+import org.springframework.web.servlet.mvc.support.*
+
+
 
 @Controller
+@SessionAttributes("items")
 class ItemsController {
 
+    @get:ModelAttribute("items")
+    val item: Set<Item>
+        get(){
+            var items = HashSet<Item>()
+            items.add(Item("Foo"))
+            items.add(Item("aaa"))
+            return items
+        }
+
     @RequestMapping("/")
-    fun indexAction():String{
+    fun indexAction(@RequestAttribute("msg") msg:String?):String{
         return "index"
+    }
+
+    @RequestMapping("/new")
+    fun newAction():String{
+        return "newForm"
+    }
+
+    @PostMapping("/addNew")
+    fun addNewAction(@ModelAttribute("nom") nom:String, @SessionAttribute("items") items:HashSet<Item>, attrs:RedirectAttributes):RedirectView{
+        items.add(Item(nom))
+        attrs.addFlashAttribute("msg", "$nom a été ajouté avec succès :)")
+        return RedirectView("/")
     }
 
 }
---
+++ src/main/kotlin/edu/spring/td1/models/Item.kt
@@ -0,0 +1,5 @@
+package edu.spring.td1.models
+
+class Item (var nom:String){
+    var evaluation:Int=0
+}
